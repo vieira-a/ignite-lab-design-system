6fbd374899d696a4026a615118b945c4958b8b68
Anderson Vieira <asvieira.br@gmail.com>
2022-10-16 00:59:36 -0300

    feat: finish components layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,59 @@
-import './styles/global.css'
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './components/Logo';
+import './styles/global.css';
+import { Envelope, Lock } from 'phosphor-react';
+import { Checkbox } from './components/Checkbox';
+import { Button } from './components/Button';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1 className='font-bold text-md text-gray-900'>Ignite Lab - Design System</h1>
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-200">
+      <header className='flex flex-col items-center'>
+        <Logo />
+        <Heading size='lg' className='text-gray-200 mt-4'>
+          Ignite Lab
+        </Heading>
+        <Text size='lg' className='text-gray-500 mt-2'>
+          Faça login e começe a usar!
+        </Text>
+      </header>
+      <form className='flex flex-col gap-4 mt-10 items-stretch w-full max-w-[400px]'>
+        <label htmlFor="email" className='flex flex-col gap-2'>
+          <Text className='font-semibold'>Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope/>
+            </TextInput.Icon>
+            <TextInput.Input type="email" id='email' placeholder='Digite o seu e-mail'/>
+          </TextInput.Root>
+        </label>
+        <label htmlFor="password" className='flex flex-col gap-2'>
+          <Text className='font-semibold'>Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock/>
+            </TextInput.Icon>
+            <TextInput.Input type="password" id="password" placeholder='********'/>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className='flex items-center gap-2'>
+          <Checkbox id='remember' />
+          <Text size='sm' className='text-gray-500'>Lembrar por 30 dias?</Text>
+        </label>
+        <Button type='submit' className='mt-4'>Entrar na plataforma</Button>
+      </form>
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-500 underline hover:text-gray-200'>Esqueceu sua senha?</a>
+        </Text>
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-500 underline hover:text-gray-200'>Não possui conta? Crie uma agora!</a>
+        </Text>
+      </footer>
     </div>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,21 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
     const Comp = asChild ? Slot: 'button';
 
     return (
         <Comp 
             className={clsx(
-                'py-4 px-3 bg-cyan-medium-light rounded text-gray-900 font-semibold text-sm w-full transition-colors hover:bg-cyan-light',
+                'py-3 px-4 bg-cyan-medium-light rounded text-gray-900 font-semibold text-sm w-full transition-colors hover:bg-cyan-light',
+                className
             )}
                 >{children}</Comp>
     )
